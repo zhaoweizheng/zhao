d9dfe8768dabf947c8a13c5b6c896a48b78d438a
lanouhn <lanouhn@lanouhndeMac-mini-5.local>
2015-01-12 08:29:33 +0800

    Initial Commit

---
+++ zhao/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  zhao
+//
+//  Created by lanouhn on 15/1/12.
+//  Copyright (c) 2015年 lanouhn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
